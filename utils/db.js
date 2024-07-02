9f2814f3b9dfe0b7828a61e8393598f9c43bba92
BnKeTia <bernardnketia51@gmail.com>
2024-07-02 23:38:24 +0000

    create db.js

---
+++ utils/db.js
@@ -0,0 +1,50 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+        console.log('MongoDB client connected to the server');
+      })
+      .catch((err) => {
+        console.error(`MongoDB client not connected to the server: ${err.message}`);
+      });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const usersCollection = this.db.collection('users');
+      const usersCount = await usersCollection.countDocuments();
+      return usersCount;
+    } catch (error) {
+      console.error(`Error getting number of users: ${error.message}`);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const filesCollection = this.db.collection('files');
+      const filesCount = await filesCollection.countDocuments();
+      return filesCount;
+    } catch (error) {
+      console.error(`Error getting number of files: ${error.message}`);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
